3eaf43b01988934bc7bfce86f2d648f89406a973
Donte <dontetruong@gmail.com>
2020-12-17 22:07:49 -0600

    Accomplishments framework

---
+++ resources/javascript/accomplishments.js
@@ -0,0 +1,17 @@
+const body = document.querySelector("body")
+const hamburger = document.querySelector(".hamburger")
+const header = document.querySelector("header")
+
+hamburger.addEventListener("click", () => {
+    body.classList.toggle("active")
+})
+
+window.addEventListener("scroll", (event) => {
+    if (window.scrollY >  40) {
+        header.style.backgroundColor = "transparent"
+        header.style.boxShadow = "none"
+    } else {
+        header.style.backgroundColor = "#16213e"
+        header.style.boxShadow = "0px 5px 10px 3px rgba(30, 30, 30, 0.5)"
+    }
+})
