aba7d6ef87eac5e33defb85e353a3b944d0018cc
Donte <dontetruong@gmail.com>
2020-12-27 19:44:23 -0600

    project cards

--- resources/javascript/default.js
+++ resources/javascript/default.js
@@ -7,11 +7,16 @@ hamburger.addEventListener("click", () => {
 })
 
 window.addEventListener("scroll", (event) => {
-    if (window.scrollY >  40) {
+    if (window.scrollY <  40) {
         header.style.backgroundColor = "transparent"
         header.style.boxShadow = "none"
     } else {
         header.style.backgroundColor = "#16213e"
         header.style.boxShadow = "0px 5px 10px 3px rgba(30, 30, 30, 0.5)"
     }
+})
+
+window.addEventListener("load", () => {
+    header.style.backgroundColor = "transparent"
+    header.style.boxShadow = "none"
 })
