5fb407fab5a2d7cb2d6d3bac3e3c49e3ef593f6b
Donte <dontetruong@gmail.com>
2021-01-04 10:01:03 -0600

    home page responsiveness addition

--- resources/javascript/index.js
+++ resources/javascript/index.js
@@ -7,11 +7,16 @@ hamburger.addEventListener("click", () => {
 })
 
 window.addEventListener("scroll", (event) => {
-    if (window.scrollY >  40) {
+    if (window.scrollY >  40 && window.innerWidth > 1000) {
         header.style.backgroundColor = "transparent"
         header.style.boxShadow = "none"
     } else {
         header.style.backgroundColor = "#16213e"
         header.style.boxShadow = "0px 5px 10px 3px rgba(30, 30, 30, 0.5)"
     }
+})
+
+window.addEventListener("resize", () => {
+    header.style.backgroundColor = "#16213e"
+    header.style.boxShadow = "0px 5px 10px 3px rgba(30, 30, 30, 0.5)"
 })
